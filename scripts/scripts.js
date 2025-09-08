060006a2d4d9671748eb647045904099c58fb80c
Brady Haviland <bradyhaviland@mail.weber.edu>
2025-09-07 20:55:40 -0600

    Javascript complete with dark, light, and default changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,37 @@
 function defaultMode() {
-
+    document.getElementById('pic').style = "background-color: rgb(92, 163, 205);";
+    document.getElementById('copyright').style = "background-color: rgb(92, 163, 205);";
+    document.getElementById('projects').style = "background-color: rgb(92, 163, 205);";
+    document.getElementById('body').style = "background-color:rgb(240, 203, 133); color: black;" ;
+    document.getElementById('h1').style = "background-color: white; color: black;" ;
+    document.getElementById('button1').style = "background-color: white; color: black;" ;
+    document.getElementById('button2').style = "background-color: white; color: black;" ;
+    document.getElementById('button3').style = "background-color: white; color: black;" ;
+    
+    
 }
-
 function darkMode() {
-
+    document.getElementById('pic').style = "background-color: rgb(2, 45, 69); color: rgb(144, 146, 147) ;" ;
+    document.getElementById('copyright').style = "background-color: rgb(2, 45, 69); color: rgb(144, 146, 147) ;" ;
+    document.getElementById('projects').style = "background-color: rgb(2, 45, 69); color: rgb(144, 146, 147) ;" ;
+    document.getElementById('body').style = "background-color: rgb(52, 35, 3); color: rgb(144, 146, 147) ;" ;
+    document.getElementById('h1').style = "background-color: black; color: rgb(144, 146, 147);" ;
+    document.getElementById('button1').style = "background-color: black; color:rgb(144, 146, 147) ;" ;
+    document.getElementById('button2').style = "background-color: black; color:rgb(144, 146, 147) ;" ;
+    document.getElementById('button3').style = "background-color: black; color:rgb(144, 146, 147) ;" ;
+    
+    
+    
 }
 
 function lightMode() {
-
+    document.getElementById('pic').style = "background-color: rgb(193, 227, 246); color: black;";
+    document.getElementById('copyright').style = "background-color: rgb(193, 227, 246); color: black;";
+    document.getElementById('projects').style = "background-color: rgb(193, 227, 246); color: black;";
+    document.getElementById('body').style = "background-color:rgb(245, 224, 186); color: black;" ;
+    document.getElementById('h1').style = "background-color: rgb(205, 207, 208); color: black;" ;
+    document.getElementById('button1').style = "background-color: rgb(205, 207, 208); color: black;" ;
+    document.getElementById('button2').style = "background-color: rgb(205, 207, 208); color: black;" ;
+    document.getElementById('button3').style = "background-color: rgb(205, 207, 208); color: black;" ;
+    
 }
